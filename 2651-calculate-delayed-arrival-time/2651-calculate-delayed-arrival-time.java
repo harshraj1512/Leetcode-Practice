82736339f22726ad5ace2d738ec81a8a74872c63
Harsh Raj <80125708+harshraj1512@users.noreply.github.com>
2023-07-13 19:20:47 +0530

    Time: 1 ms (72.95%), Space: 39.8 MB (36.22%) - LeetHub

---
+++ 2651-calculate-delayed-arrival-time/2651-calculate-delayed-arrival-time.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int findDelayedArrivalTime(int arrivalTime, int delayedTime) {
+        int ans = arrivalTime + delayedTime;
+
+        if(ans >= 24){
+            return ans - 24;
+        }
+
+        return ans;
+    }
+}
