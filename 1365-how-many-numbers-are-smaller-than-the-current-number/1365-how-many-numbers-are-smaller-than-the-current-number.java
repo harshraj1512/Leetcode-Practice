9ad540aff013af9636e59d74576bb9def6e81e82
Harsh Raj <80125708+harshraj1512@users.noreply.github.com>
2023-09-02 01:53:37 +0530

    Time: 1 ms (99.96%), Space: 43.9 MB (31.27%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.java
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.java
@@ -1,12 +1,22 @@
 class Solution {
     public int[] smallerNumbersThanCurrent(int[] nums) {
         int[] ans = new int[nums.length];
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = 0; j < ans.length; j++) {
-                if(nums[j] < nums[i]){
-                    ans[i]++;
-            }    }
-        
+        int[] freq = new int[101];
+        // frequency
+        for(int i = 0; i< nums.length; i++){
+            freq[nums[i]]++;
+        }
+        // running sum
+        for(int i = 1; i<101; i++){
+            freq[i] += freq[i-1]; 
+        }
+        //
+        for(int i = 0; i< nums.length;i++){
+            if(nums[i] == 0){
+                ans[i] = 0;
+            } else {
+                ans[i] = freq[nums[i]-1];
+            }
         }
         return ans;
     }
