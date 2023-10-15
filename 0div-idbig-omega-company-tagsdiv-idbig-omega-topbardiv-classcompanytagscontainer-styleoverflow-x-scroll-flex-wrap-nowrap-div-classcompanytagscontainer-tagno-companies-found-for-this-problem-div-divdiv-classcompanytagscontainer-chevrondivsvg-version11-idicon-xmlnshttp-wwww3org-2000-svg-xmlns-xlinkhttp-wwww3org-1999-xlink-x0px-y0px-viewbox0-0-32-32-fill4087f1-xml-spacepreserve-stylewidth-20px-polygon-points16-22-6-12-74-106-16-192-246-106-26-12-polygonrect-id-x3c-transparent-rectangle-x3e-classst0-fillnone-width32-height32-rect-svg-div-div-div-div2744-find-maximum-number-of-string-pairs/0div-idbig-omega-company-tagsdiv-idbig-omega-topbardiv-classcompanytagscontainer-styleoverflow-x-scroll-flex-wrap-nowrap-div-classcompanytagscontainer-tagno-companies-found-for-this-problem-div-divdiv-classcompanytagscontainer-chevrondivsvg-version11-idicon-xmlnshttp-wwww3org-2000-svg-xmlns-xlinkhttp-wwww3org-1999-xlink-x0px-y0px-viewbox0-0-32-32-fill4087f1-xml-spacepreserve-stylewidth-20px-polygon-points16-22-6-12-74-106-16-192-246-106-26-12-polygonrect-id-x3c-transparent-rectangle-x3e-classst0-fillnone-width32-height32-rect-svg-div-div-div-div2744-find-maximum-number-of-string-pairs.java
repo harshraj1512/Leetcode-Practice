db697988754fef608401ec6b1e6705cac569d09f
Harsh Raj <80125708+harshraj1512@users.noreply.github.com>
2023-10-15 21:04:09 +0530

    Time: 3 ms (62.49%), Space: 43 MB (46.93%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2744-find-maximum-number-of-string-pairs/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2744-find-maximum-number-of-string-pairs.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maximumNumberOfStringPairs(String[] words) {
+        int count =0;
+        HashMap<String,Integer> arr = new HashMap<>();
+
+        for(int i=0 ;i<words.length;i++){
+             String r = new StringBuilder(words[i]).reverse().toString();
+            if(arr.containsKey(r)){
+                arr.put(r, arr.get(r)+1);
+            }
+            else{
+                arr.put(words[i],0);
+            }
+        }
+
+        for(int j : arr.values()){
+            count += j;
+        }
+        return count;
+    }
+}
